d723da9adb02f5f474cd205d693c7e1e8bc5dd2a
oh <outputor777@gmail.com>
2022-11-03 16:00:37 +0900

    useInput

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import useInput from './hooks/useInput';
 
 function App() {
+  const validator = (value) => value.length <= 10;
+  const name = useInput('', validator);
+  const nickname = useInput('', validator);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <input type="text" placeholder="Name" {...name} />
+      <input type="text" placeholder="Nickname" {...nickname} />
     </div>
   );
 }
---
+++ src/hooks/useInput.jsx
@@ -0,0 +1,20 @@
+import React, { useCallback, useState } from 'react'
+
+const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue);
+  // change
+  const onChange = useCallback((event) => {
+    const {value} = event.target;
+    let willUpdate = true;
+    if(typeof validator === 'function') {
+      willUpdate = validator(value);
+    }
+    if(willUpdate) {
+      setValue(value);
+    }
+  }, []);
+  
+  return {value, onChange};
+}
+
+export default useInput;
